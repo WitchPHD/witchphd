d64f93c7eb7336780b67d10e62329b77cde0143f
WitchPHD <74690105+WitchPHD@users.noreply.github.com>
2026-05-05 22:00:06 -0400

    Add files via upload

--- ping.js
+++ ping.js
@@ -1,6 +1,6 @@
 /* Small javascript for displaying page load time */
 
 window.addEventListener('load', () => {
-    const loadTime = performance.now();
-    document.querySelector('.ping').innerHTML = `[${loadTime.toFixed(0)}ms]`;
+        const loadTime = performance.now();
+        document.querySelector('.ping').innerHTML = `[${loadTime.toFixed(0)}ms]`;
 });
